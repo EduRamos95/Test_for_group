3dc1c65f06156951c7e965b3fdbe2b5148d3b6a1
EduRamos95 <eduramos08.30@gmail.com>
2022-08-20 19:57:46 -0500

    last_node listo

--- MONTY_2/last_node.c
+++ MONTY_2/last_node.c
@@ -1,36 +1,34 @@
 #include "monty.h"
 /**
- * last_node - top of stack.
- * 
- * @stack: pointer to head of stack; 
- * Return: stack_t* 
+ * last_node - top of stack
+ * @stack: pointer to head of stack
+ * Return: pointer to top
  */
 stack_t *last_node(stack_t **stack)
 {
-    stack_t *top;
-    if(*stack == NULL)
-        return (NULL);
-    top = *stack;
-    while (top->next != NULL)
-        top = top->next;
+	stack_t *top;
 
-    return (top);
+	if (*stack == NULL)
+		return (NULL);
+	top = *stack;
+	while (top->next != NULL)
+		top = top->next;
+	return (top);
 }
 
 /**
- * first_node - bot of stack.
- * 
- * @stack: pointer to top of stack; 
- * Return: stack_t* 
+ * first_node - bot of stack
+ * @stack: pointer to top of stack
+ * Return: pointer to head of stack
  */
 stack_t *first_node(stack_t **stack)
 {
-    stack_t *bot;
-    if(*stack == NULL)
-        return (NULL);
-    bot = *stack;
-    while (bot->prev != NULL)
-        bot = bot->prev;
+	stack_t *bot;
 
-    return (bot);
+	if (*stack == NULL)
+		return (NULL);
+	bot = *stack;
+	while (bot->prev != NULL)
+		bot = bot->prev;
+	return (bot);
 }
