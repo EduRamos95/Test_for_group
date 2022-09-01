ab0618947c89f9dd46eb3b82de3fb8f2d56bad31
EduRamos95 <eduramos08.30@gmail.com>
2022-09-01 00:38:21 -0500

    task 14

--- Binary_trees/binary_trees.h
+++ Binary_trees/binary_trees.h
@@ -55,7 +55,7 @@ size_t binary_tree_size(const binary_tree_t *tree);
 /*task 12*/
 size_t binary_tree_leaves(const binary_tree_t *tree);
 /*task 13*/
-size_t binary_tree_nodes(const binary_tree_t
+size_t binary_tree_nodes(const binary_tree_t *tree);
 /*task 14*/
 int binary_tree_balance(const binary_tree_t *tree);
 /*task 15*/
