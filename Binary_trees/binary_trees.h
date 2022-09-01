15dc982ca3fd783c4ce3aa81fbe30e32e870f2c1
EduRamos95 <eduramos08.30@gmail.com>
2022-09-01 17:38:17 -0500

    task19

--- Binary_trees/binary_trees.h
+++ Binary_trees/binary_trees.h
@@ -109,7 +109,7 @@ avl_t *sorted_array_to_avl(int *array, size_t size);
 /*task 36*/
 int binary_tree_is_heap(const binary_tree_t *tree);
 /*task 37*/
-heap_t *heap_insert(heap_t **root, int value)
+heap_t *heap_insert(heap_t **root, int value);
 /*task 38*/
 heap_t *array_to_heap(int *array, size_t size);
 /*task 39*/
