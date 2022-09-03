5acc81b72206cc493fc303c9c81c7d0e6e60cb83
EduRamos95 <eduramos08.30@gmail.com>
2022-09-03 16:24:19 -0500

    for all task heap

--- Binary_trees/binary_trees.h
+++ Binary_trees/binary_trees.h
@@ -124,4 +124,12 @@ int heap_extract(heap_t **root);
 int *heap_to_sorted_array(heap_t *heap, size_t *size);
 /*task 41*/
 /*File Big O #BinaryHeap*/
+
+/*some helper functions*/
+avl_t *rebalance(avl_t *node, avl_t **tree);
+avl_t *balance_left(avl_t *node);
+avl_t *balance_right(avl_t *node);
+char *convert(unsigned long int num, int base, int lowercase);
+bst_t *swap(bst_t *a, bst_t *b);
+
 #endif
